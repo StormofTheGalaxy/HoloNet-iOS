afa0d5311829d401b1b38da267f28cb93bab5d51
Gru <ruslru376@gmail.com>
2024-09-19 00:35:45 +1000

    ahhh wat

--- HoloNet/AppDelegate.swift
+++ HoloNet/AppDelegate.swift
@@ -137,7 +137,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let dataDict:[String: String] = ["token": fcmToken ?? ""]
         NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
-        handleFCMToken()
+          // I handle that on JS side
+          // handleFCMToken()
         // TODO: If necessary send token to application server.
         // Note: This callback is fired at each app startup and whenever a new token is generated.
       }
