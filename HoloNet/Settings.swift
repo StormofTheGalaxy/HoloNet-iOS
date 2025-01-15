a748e17ed49a916b350ca91d62bf4be5c6d7c485
Gru <ruslru376@gmail.com>
2025-01-15 16:42:38 +1000

    CRM Beta App

--- HoloNet/Settings.swift
+++ HoloNet/Settings.swift
@@ -9,15 +9,15 @@ struct Cookie {
 let gcmMessageIDKey = "00000000000" // update this with actual ID if using Firebase
 
 // URL for first launch
-let rootUrl = URL(string: "https://holonet.stormgalaxy.com")!
+let rootUrl = URL(string: "https://crm.stormgalaxy.com")!
 
 // allowed origin is for what we are sticking to pwa domain
 // This should also appear in Info.plist
-let allowedOrigins: [String] = ["holonet.stormgalaxy.com", "appleid.apple.com", "apple.com"]
+let allowedOrigins: [String] = ["crm.stormgalaxy.com"]
 
 // auth origins will open in modal and show toolbar for back into the main origin.
 // These should also appear in Info.plist
-let authOrigins: [String] = ["discord.com", "github.com", "vk.com"]
+let authOrigins: [String] = []
 // allowedOrigins + authOrigins <= 10
 
 let platformCookie = Cookie(name: "app-platform", value: "iOS App Store")
@@ -27,4 +27,4 @@ let displayMode = "standalone" // standalone / fullscreen.
 let adaptiveUIStyle = true     // iOS 15+ only. Change app theme on the fly to dark/light related to WebView background color.
 let overrideStatusBar = true   // iOS 13-14 only. if you don't support dark/light system theme.
 let statusBarTheme = "dark"    // dark / light, related to override option.
-let pullToRefresh = false    // Enable/disable pull down to refresh page
+let pullToRefresh = true    // Enable/disable pull down to refresh page
